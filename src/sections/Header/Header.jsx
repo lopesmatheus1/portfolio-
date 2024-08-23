9128f28b9ea962bd0346b08779a1fd6b3d88902b
Matheus Lopes Marendino <lopesmatheus1@hotmail.com>
2024-08-23 09:47:57 -0300

    feat: adding scroll smooth

--- src/sections/Header/Header.jsx
+++ src/sections/Header/Header.jsx
@@ -3,7 +3,7 @@ import List from '../../assets/icons/list.svg'
 import { StyledHeader,NavbarMobile,HeaderMobile} from './StyledHeader'
 import { Drawer } from '@mui/material';
 import React, { useState } from 'react'
-
+import { Link } from 'react-scroll';
 
 
 function Header() {
@@ -19,14 +19,16 @@ function Header() {
           <img src={Logo} alt="" />
         </a>
         <nav>
-          <a href="#Hero">Home</a>
-          <a href="#Soft">Soft Skills</a>
-          <a href="#HardSkills">Hard Skils</a>
-          <a href="#Projetos">Projetos</a>
+          <Link to="Hero" smooth={true} duration={800} style={{cursor:'pointer'}}>Home</Link>
+          <Link to="Soft" smooth={true} duration={800} style={{cursor:'pointer'}}>Soft Skills</Link>
+          <Link to="HardSkills" smooth={true} duration={800} style={{cursor:'pointer'}}>Hard Skills</Link>
+          <Link to="Projetos" smooth={true} duration={800} style={{cursor:'pointer'}}>Projetos</Link>
           
         </nav>
 
-        <nav><a href="#Contact">Contatos</a></nav>
+        <nav>
+          <Link to='Contact' smooth={true} duration={800} style={{cursor:'pointer'}}>Contatos</Link>
+          </nav>
         <HeaderMobile>
           <img src={List} alt="" onClick={handleOpenMenu} />
         </HeaderMobile>
